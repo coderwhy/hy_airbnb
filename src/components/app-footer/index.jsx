92f123058bd04861ad58a60b6bdf7ccf5df2b18b
coderwhy <coderwhy@gmail.com>
2022-09-21 18:06:36 +0800

    解决eslint的问题

--- src/components/app-footer/index.jsx
+++ src/components/app-footer/index.jsx
@@ -3,6 +3,8 @@ import { FooterWrapper } from './style'
 import footerData from "@/assets/data/footer.json"
 
 const AppFooter = memo(() => {
+  console.log("AppFooter")
+
   return (
     <FooterWrapper>
       <div className='wrapper'>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
